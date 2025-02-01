c693b675f064962b480710a6dac2ba2f6a628e48
Otydnaandyto <otydna1307@gmail.com>
2025-02-01 14:20:33 +0700

    tess commit

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+require('dotenv').config();
+
+// import module yang dibutuhkan
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
@@ -7,7 +10,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
-var app = express(); 
+var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -20,15 +23,15 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter); //gunakan indexRouter untuk route '/' / root
-app.use('/users', usersRouter); 
+app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -38,14 +41,12 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-
 // Set port
-const port = process.env.APP_PORT || 4000;  
+const port = process.env.APP_PORT || 4000;
 
 // Start server
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
-});     //jalankan server pada port yang telah ditentukan
+}); //jalankan server pada port yang telah ditentukan
 
 module.exports = app;
-
--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,4 @@
 var express = require('express'); //import express
 var router = express.Router(); //import router dari express
-const { pool } = require('../database/db'); //import pool dari db.js 
-
-// GET all users
-router.get('/', async (req, res) => { 
-  try {
-    const result = await pool.query('SELECT * FROM users');
-    res.json(result.rows);
-  } catch (err) {
-    console.error(err);
-    res.status(500).send(err.message);
-  }
-});
 
 module.exports = router;
